fb2d7ba4ee1f4c641ec6bfdc99bdc91b68467482
matthew-e-thomas <matt.thomas@email.virginia.edu>
2019-11-20 17:19:53 -0500

    added LDA, QDA, and KNN model

--- Haiti_Project.R
+++ Haiti_Project.R
@@ -1,6 +1,11 @@
 Haiti <- read.csv("HaitiPixels.csv", header=TRUE)
 
+#Hold Out Data Import and Clean Up
+Tarp_67 <- read.csv("Hold+Out+Data/067Blue_Tarps.csv", header=T)
+Tarp_67$tarp <- "Yes"
+
 library(glmnet)
+library(MASS)
 library(caret)
 library(dplyr)
 set.seed(1001)
@@ -13,9 +18,10 @@ tarp_y <- as.factor(tarp_y)
 
 myFolds <- createFolds(tarp_y, k = 10)
 
-# Create reusable trainControl object: myControl
+# Create reusable trainControl object to compare models
 myControl <- trainControl(
   summaryFunction = twoClassSummary,
+  method="cv",
   classProbs = TRUE,
   verboseIter = TRUE,
   savePredictions = TRUE,
@@ -32,3 +38,29 @@ model_logistic <- train(
   method = "glmnet",
   trControl = myControl
 )
+
+#LDA Model
+model_lda <- train(
+  x = tarp_x,
+  y = tarp_y,
+  method="lda", 
+  trControl = myControl
+)
+
+
+#QDA Model
+model_qda <- train(
+  x = tarp_x,
+  y = tarp_y,
+  method="qda", 
+  trControl = myControl
+)
+
+#KNN Model
+model_knn <- train(
+  x = tarp_x,
+  y = tarp_y, 
+  method = "knn", 
+  trControl = myControl, 
+  tuneLength = 20
+  )
