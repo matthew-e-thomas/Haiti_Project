c5063e57fb2d6635f12ef86b7dbe1c05e0e469d6
matthew-e-thomas <matt.thomas@email.virginia.edu>
2019-11-21 16:33:44 -0500

    Added some comparisons between two and multi factor models

--- Haiti_Project.R
+++ Haiti_Project.R
@@ -2,7 +2,10 @@ Haiti <- read.csv("HaitiPixels.csv", header=TRUE)
 
 #Hold Out Data Import and Clean Up
 Tarp_67 <- read.csv("Hold+Out+Data/067Blue_Tarps.csv", header=T)
-Tarp_67$tarp <- "Yes"
+Tarp_67$tarp <- "Yes" #need to add column for whether it's a blue tarp or not
+Tarp_67Not <- read.csv("Hold+Out+Data/067Not_Blue_Tarps.csv", header=T)
+Tarp_67Not$tarp <- "No"
+Test_Data <- rbind(Tarp_67, Tarp_67Not)
 
 library(glmnet)
 library(MASS)
@@ -12,11 +15,17 @@ set.seed(1001)
 
 Haiti$tarp <- ifelse(Haiti$Class == "Blue Tarp", "Yes", "No")
 
+#Fits for two class models
 tarp_y <- Haiti$tarp
 tarp_x <- Haiti[, 2:4]
 tarp_y <- as.factor(tarp_y)
 
-myFolds <- createFolds(tarp_y, k = 10)
+#fits for multi class models
+tarp_y_multi <- Haiti$Class
+tarp_x <- Haiti[, 2:4]
+tarp_y_multi <- as.factor(tarp_y_multi)
+
+myFolds <- createFolds(tarp_y_multi, k = 10)
 
 # Create reusable trainControl object to compare models
 myControl <- trainControl(
@@ -28,16 +37,27 @@ myControl <- trainControl(
   index = myFolds
 )
 
-levels(tarp_y) <- c("Blue.Tarp", "Rooftop", "Soil", "Various.Non.Tarp",
+#trainControl for multi class
+myControl_multi <- trainControl(
+  summaryFunction = multiClassSummary,
+  method="cv",
+  classProbs = TRUE,
+  verboseIter = TRUE,
+  savePredictions = TRUE,
+  index = myFolds
+)
+
+levels(tarp_y_multi) <- c("Blue.Tarp", "Rooftop", "Soil", "Various.Non.Tarp",
                     "Vegetation")
 
 model_logistic <- train(
   x = tarp_x, 
-  y = tarp_y,
-  metric = "ROC",
+  y = tarp_y_multi,
+  metric = "Specificity",
   method = "glmnet",
   trControl = myControl
 )
+confusionMatrix(model_logistic, "none")
 
 #LDA Model
 model_lda <- train(
